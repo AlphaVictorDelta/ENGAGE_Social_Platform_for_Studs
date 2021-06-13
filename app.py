e63d11f918854f7ec75a13ef46b7ed23b9bf81e2
Adarsh Desai <desaiadarsh22@gmail.com>
2021-06-13 20:56:41 +0530

    Converted forms to WTF Forms

--- app.py
+++ app.py
@@ -44,7 +44,9 @@ def timeline():
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     manager.run()
