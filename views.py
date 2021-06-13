85fba47aa44bf7295e4a8c0069301da14381e942
Adarsh Desai <desaiadarsh22@gmail.com>
2021-06-13 21:17:01 +0530

    Made the timeline more general

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from app import app, photos, db
 from models import User, Tweet
 from forms import RegisterForm, LoginForm, TweetForm
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import login_required, login_user, current_user, logout_user
@@ -45,11 +45,21 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/timeline')
-def timeline():
+@app.route('/timeline', defaults={'username' : None})
+@app.route('/timeline/<username>')
+def timeline(username):
     form = TweetForm()
 
-    user_id = current_user.id
+    if username:
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            abort(404)
+
+        user_id = user.id
+    else:
+        user = current_user
+        user_id = current_user.id
+
     tweets = Tweet.query.filter_by(user_id=user_id).order_by(Tweet.date_created.desc()).all()
     total_tweets = len(tweets)
 
@@ -57,7 +67,7 @@ def timeline():
 
 
 
-    return render_template('timeline.html', form=form, tweets=tweets, current_time=current_time, current_user=current_user, total_tweets=total_tweets)
+    return render_template('timeline.html', form=form, tweets=tweets, current_time=current_time, current_user=user, total_tweets=total_tweets)
 
 @app.route('/post_tweet', methods=['POST'])
 @login_required
